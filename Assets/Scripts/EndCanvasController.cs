342246342c7170c1b3c85e4726c809661db47e77
benhepburn <ben.h@homeofpmg.com>
2025-10-07 13:57:05 +1100

    end canvas

---
+++ Assets/Scripts/EndCanvasController.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class EndCanvasController : MonoBehaviour {
+    [SerializeField] private SplineAnimate splineAnimator;
+    private Canvas _canvas;
+
+    private void OnEnable() {
+        if (splineAnimator == null) splineAnimator = GetComponent<SplineAnimate>();
+        if (_canvas == null) _canvas = GetComponent<Canvas>();
+        
+        splineAnimator.Completed += HandleSplineCompleted;
+    }
+    
+    private void OnDisable() {
+        splineAnimator.Completed -= HandleSplineCompleted;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {
+        _canvas.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update() { }
+
+    private void HandleSplineCompleted() {
+        _canvas.enabled = true;
+    }
+}
