a4bb9b6ee5b9f5bd0c3e1e130e4493847c9e2ab0
maizedw <maize@maizewallin.com>
2026-01-15 16:43:22 +1100

    polish functionality for randomisation

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 using Unity.Tutorials.Core.Editor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 public class GameController : MonoBehaviour {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -9,17 +10,28 @@ public class GameController : MonoBehaviour {
     // Update is called once per frame
     void Update() { }
 
+    public bool IsRandom;
+    private string _oldSceneName;
+    public int NumberOfScenes;
+    
     public void LoadScene(string sceneName) {
-        if (sceneName.IsNullOrEmpty()) sceneName = SceneManager.GetActiveScene().name;
-        else if (sceneName == "random")
+        if (sceneName.IsNullOrEmpty() && IsRandom == false) sceneName = SceneManager.GetActiveScene().name;
+        else if (sceneName == "random" || IsRandom)
         {
-            int i = Random.Range(0, 2);
+            if (sceneName != "random") _oldSceneName = sceneName;
+            sceneName = "random";
+            int i = Random.Range(0, NumberOfScenes);
+            if (_oldSceneName.IsNotNullOrEmpty())
+            {
+                LSLController.Instance.LogControl($"GameController is set to load a random scene on restart. " +
+                                                  $" if you are looking to load {_oldSceneName} make sure to uncheck that box!");
+            }
             SceneManager.LoadScene(i);
         }
  
         AkUnitySoundEngine.StopAll();
+        if (sceneName != "random" || !IsRandom) SceneManager.LoadScene(sceneName);
         LSLController.Instance.LogControl($"Loading scene {sceneName}");
-        if (sceneName != "random") SceneManager.LoadScene(sceneName);
     
     }
 }
